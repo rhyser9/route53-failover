7bb44b41b1796ddac52d370f42e694b8ee8a113e
Rhys Barrie <rhys.barrie@gmail.com>
2022-12-04 19:51:07 -0500

    Add /sites endpoint

--- lib/account.ts
+++ lib/account.ts
@@ -1,4 +1,5 @@
 import { GetCallerIdentityCommand, STSClient } from "@aws-sdk/client-sts";
+import { getAccountZoneIndex } from "./aws_r53Client";
 
 export async function getAccountIdFromAccessKey(access_key: string, secret_key: string): Promise<string> {
   const clientConfig = {
@@ -10,4 +11,16 @@ export async function getAccountIdFromAccessKey(access_key: string, secret_key: 
   };
   const sts = new STSClient(clientConfig);
   return (await sts.send(new GetCallerIdentityCommand({}))).Account as string;
+}
+
+export async function getAccountIdFromZoneId(zone_id: string): Promise<string | undefined> {
+  const index = await getAccountZoneIndex();
+
+  for (const [id, zones] of Object.entries(index)) {
+    if (Object.keys(zones).includes(zone_id)) {
+      return id;
+    }
+  }
+
+  return undefined;
 }
---
+++ pages/api/sites/index.ts
@@ -0,0 +1,100 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Site } from "@prisma/client";
+import prisma from "@lib/prisma";
+import { getHostedZones, getRecords } from "@lib/aws_r53Client";
+import { getAccountIdFromZoneId } from "@lib/account";
+import { Prisma } from "@prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Site[]>
+) {
+  const sites: Site[] = await prisma.site.findMany({});
+
+  switch (req.method) {
+    case 'GET': {
+      res.status(200).json(sites);
+      return;
+    }
+    case 'POST': {
+      // TODO: Validate / sanitize input
+      const data = req.body;
+      const zone_id = data.zone_id;
+      const name = data.name;
+      const fqdn = data.fqdn;
+      const notif_email = data.notif_email;
+      const notif_teams = data.notif_teams;
+      const monitoring_link = data.monitoring_link;
+
+      // TODO: validate basic fields
+      // TODO: make sure fqdn has trailing dot
+
+
+      // check unique site
+      const site = sites.find(site => site.name === name || site.fqdn === fqdn);
+      if (site !== undefined) {
+        res.status(409).end(`Site '${site.name}' (${site.fqdn}) already exists`);
+        return;
+      }
+
+      const account_id = await getAccountIdFromZoneId(zone_id);
+      if (account_id === undefined) {
+        res.status(500).end(`Couldn't find parent account for zone ID ${zone_id}`);
+        return;
+      }
+
+      const zone = (await getHostedZones(account_id))[zone_id];
+      if (zone === undefined) {
+        res.status(400).end(`Invalid hosted zone ID ${zone_id}`);
+        return;
+      }
+
+      // Check that A / AAAA / CNAME records exist in AWS
+      // TODO: check that the records are weighted? via `if typeof record.Weight !== undefined`
+      const records = await getRecords(account_id, zone_id);
+      const filteredRecordNames = records.reduce((names, record) => {
+        if (["A", "AAAA", "CNAME"].includes(record.Type ?? "")) names.push(record.Name ?? "");
+        return names;
+      }, [] as string[]);
+
+      if (!filteredRecordNames.includes(fqdn)) {
+        res.status(400).end(`No DNS records exist in AWS for ${fqdn}`);
+        return;
+      }
+
+      // insert site into database
+      try {
+        const newSiteInput: Prisma.SiteCreateInput = {
+          name: name,
+          fqdn: fqdn,
+          zone_id: zone_id,
+          account: {
+            connect: {
+              id: account_id
+            }
+          },
+          notif_email: notif_email,
+          notif_teams: notif_teams,
+          monitoring_link: monitoring_link,
+          enabled: true
+        };
+        const newSite = await prisma.site.create({
+          data: newSiteInput
+        });
+
+        res.setHeader('Location', `/api/sites/${newSite.id}`);
+        res.status(201).json([newSite]);
+        return;
+
+      } catch (e) {
+        console.log(e);
+        res.status(500).end(`Error inserting new site ${name} (${fqdn}) into database`);
+        return;
+      }
+    }
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).end(`Method ${req.method} not allowed`);
+      return;
+  }
+}
